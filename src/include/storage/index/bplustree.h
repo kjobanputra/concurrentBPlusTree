3ff9a7bc39d9f0abef923f68f57c7abe5bc9fa5b
Cal Lavicka <cal.lavicka@gmail.com>
2020-02-23 15:44:03 -0500

    Structure for b-plus tree

--- src/include/storage/index/bplustree.h
+++ src/include/storage/index/bplustree.h
@@ -1,12 +1,36 @@
 #pragma once
 
 #include <functional>
+#include "common/spin_latch.h"
 
 namespace terrier::storage::index {
+
+constexpr uint32_t NUM_CHILDREN = 5;
 
 template <typename KeyType, typename ValueType, typename KeyComparator = std::less<KeyType>,
           typename KeyEqualityChecker = std::equal_to<KeyType>, typename KeyHashFunc = std::hash<KeyType>,
           typename ValueEqualityChecker = std::equal_to<ValueType>>
-class BPlusTree {};
+class BPlusTree {
+ private:
+
+  class LeafNode {
+    KeyType keys_[NUM_CHILDREN];
+    ValueType values_[NUM_CHILDREN];
+    LeafNode *prev_;
+    LeafNode *next_;
+  };
+  class InteriorNode {
+    KeyType guideposts_[NUM_CHILDREN - 1];
+    bool leaf_children_;
+    union {
+      InteriorNode *interiors_[NUM_CHILDREN];
+      LeafNode *leaves_[NUM_CHILDREN];
+    };
+  };
+
+  InteriorNode *root_;
+  uint32_t depth_;
+  common::SpinLatch guard_;
+};
 
 }  // namespace terrier::storage::index
