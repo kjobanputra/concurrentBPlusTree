80d73ba2a2887c9a81ce56e9c4fd98c662aaedb9
Cal Lavicka <cal.lavicka@gmail.com>
2020-02-24 15:37:59 -0500

    Scans implemented via LeafNodeIterator

--- src/include/storage/index/bplustree.h
+++ src/include/storage/index/bplustree.h
@@ -35,6 +35,105 @@ class BPlusTree {
   common::SpinLatch guard_;
 
  public:
+  class LeafNodeIterator {
+   private:
+    friend class BPlusTree;
+
+    LeafNode *current_;
+    uint32_t index_;
+
+    LeafNodeIterator(LeafNode *current, uint32_t index): current_(current), index_(index) {}
+
+   public:
+    inline ValueType &Value() const {
+      TERRIER_ASSERT(!IsEnd(), "Value called on invalid iterator");
+      return current_->values_[index_];
+    }
+
+    inline const KeyType &Key() const {
+      TERRIER_ASSERT(!IsEnd(), "Key called on invalid iterator");
+      return current_->keys[index_];
+    }
+
+    inline void operator++() {
+      index_++;
+      if (index_ >= current_->filled_keys_) {
+        current_ = current_->next_;
+        index_ = 0;
+      }
+    }
+
+    inline LeafNodeIterator operator++(int) {
+      LeafNodeIterator copy = *this;
+      this->operator++();
+      return copy;
+    }
+
+    inline void operator--() {
+      if (index_ == 0) {
+        current_ = current_->prev_;
+        index_ = current_->filled_keys_ - 1;
+      } else {
+        index_--;
+      }
+    }
+
+    inline LeafNodeIterator operator--(int) {
+      LeafNodeIterator copy = *this;
+      this->operator--();
+      return copy;
+    }
+
+    inline bool operator==(const LeafNodeIterator &other) const {
+      return other.current_ == this->current_ && other.index_ == this->index_;
+    }
+
+    inline bool IsEnd() const {
+      return current_ == nullptr;
+    }
+  };
+
+  /**
+   * Gets an iterator pointing to the first key/value pair in the tree
+   * @return the iterator
+   */
+  LeafNodeIterator Begin() const {
+    InteriorNode *current = root_;
+    for (uint32_t current_depth = 0; current_depth < depth_ - 1; current_depth++) {
+      current = current->interiors_[0];
+    }
+    return {current->leaves_[0], 0};
+  }
+
+
+  /**
+   * Gets an iterator pointing to the first key/value pair in the tree, whose key is >= the key passed in.
+   * If the passed in key is larger than all keys in the tree, returns the end iterator.
+    * @param key the Lower bound (inclusive) for the
+   * @return the iterator
+   */
+  LeafNodeIterator Begin(KeyType key) const {
+    InteriorNode *current = root_;
+    LeafNode *leaf = nullptr;
+    for (uint32_t current_depth = 0; current_depth < depth_; current_depth++) {
+      uint32_t child;
+      for (child = 0; child < current->filled_guide_posts_
+        && !KeyComparator(key, current->guide_posts_[child]); child++);
+      if (current_depth == depth_ - 1) {
+        leaf = current->leaves_[child];
+      } else {
+        current = current->interiors_[child];
+      }
+    }
+    TERRIER_ASSERT(leaf != nullptr, "Leaf should be reached");
+    for (uint32_t index = 0; index < leaf->filled_keys_; index++) {
+      if (!KeyComparator(leaf->keys_[index], key)) {
+        return {leaf, index};
+      }
+    }
+    LeafNodeIterator ret = {leaf, leaf->filled_keys_ - 1};
+    return ++ret;
+  }
 };
 
 }  // namespace terrier::storage::index
--- src/include/storage/index/bplustree_index.h
+++ src/include/storage/index/bplustree_index.h
@@ -136,14 +136,12 @@ class BPlusTreeIndex final : public Index {
     index_key.SetFromProjectedRow(key, metadata_, metadata_.GetSchema().GetColumns().size());
 
     // Perform lookup in BPlusTree
-    // FIXME(15-721 project2): perform a lookup of the underlying data structure of the key
+    auto scan_itr = bplustree_->Begin(index_key);
 
-    // Avoid resizing our value_list, even if it means over-provisioning
-    value_list->reserve(results.size());
-
-    // Perform visibility check on result
-    for (const auto &result : results) {
-      if (IsVisible(txn, result)) value_list->emplace_back(result);
+    while (!scan_itr.IsEnd() && scan_itr.Key() == index_key) {
+      // Perform visibility check on result
+      if (IsVisible(txn, scan_itr.Value())) value_list->emplace_back(scan_itr.Value());
+      ++scan_itr;
     }
 
     TERRIER_ASSERT(!(metadata_.GetSchema().Unique()) || (metadata_.GetSchema().Unique() && value_list->size() <= 1),
@@ -166,7 +164,15 @@ class BPlusTreeIndex final : public Index {
     if (low_key_exists) index_low_key.SetFromProjectedRow(*low_key, metadata_, num_attrs);
     if (high_key_exists) index_high_key.SetFromProjectedRow(*high_key, metadata_, num_attrs);
 
-    // FIXME(15-721 project2): perform a lookup of the underlying data structure of the key
+    auto scan_itr = low_key_exists ? bplustree_->Begin(index_low_key) : bplustree_->Begin();
+
+    // Limit of 0 indicates "no limit"
+    while ((limit == 0 || value_list->size() < limit) && !scan_itr.IsEnd() &&
+           (!high_key_exists || scan_itr.Key().PartialLessThan(index_high_key, &metadata_, num_attrs))) {
+      // Perform visibility check on result
+      if (IsVisible(txn, scan_itr.Value())) value_list->emplace_back(scan_itr.Value());
+      ++scan_itr;
+    }
   }
 
   void ScanDescending(const transaction::TransactionContext &txn, const ProjectedRow &low_key,
@@ -178,7 +184,16 @@ class BPlusTreeIndex final : public Index {
     index_low_key.SetFromProjectedRow(low_key, metadata_, metadata_.GetSchema().GetColumns().size());
     index_high_key.SetFromProjectedRow(high_key, metadata_, metadata_.GetSchema().GetColumns().size());
 
-    // FIXME(15-721 project2): perform a lookup of the underlying data structure of the key
+    // Perform lookup in BwTree
+    auto scan_itr = bplustree_->Begin(index_high_key);
+    // Back up one element if we didn't match the high key
+    if (scan_itr.IsEnd() || scan_itr.Key() > index_high_key) --scan_itr;
+
+    while (!(scan_itr.IsEnd() || scan_itr.Key() < index_low_key)) {
+      // Perform visibility check on result
+      if (IsVisible(txn, scan_itr.Value())) value_list->emplace_back(scan_itr.Value());
+      --scan_itr;
+    }
   }
 
   void ScanLimitDescending(const transaction::TransactionContext &txn, const ProjectedRow &low_key,
@@ -192,7 +207,16 @@ class BPlusTreeIndex final : public Index {
     index_low_key.SetFromProjectedRow(low_key, metadata_, metadata_.GetSchema().GetColumns().size());
     index_high_key.SetFromProjectedRow(high_key, metadata_, metadata_.GetSchema().GetColumns().size());
 
-    // FIXME(15-721 project2): perform a lookup of the underlying data structure of the key
+    // Perform lookup in BwTree
+    auto scan_itr = bplustree_->Begin(index_high_key);
+    // Back up one element if we didn't match the high key
+    if (scan_itr.IsEnd() || scan_itr.Key() > index_high_key) --scan_itr;
+
+    while (value_list->size() < limit && !(scan_itr.IsEnd() || scan_itr.Key() < index_low_key)) {
+      // Perform visibility check on result
+      if (IsVisible(txn, scan_itr.Value())) value_list->emplace_back(scan_itr.Value());
+      --scan_itr;
+    }
   }
 };
 
